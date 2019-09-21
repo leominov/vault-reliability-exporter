06f279cf71e266403d767279faf8957702f6d97f
l.aminov <l.aminov@tinkoff.ru>
2019-09-21 11:37:43 +0500

    Switch to HistogramVec

--- exporter.go
+++ exporter.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"strings"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -19,31 +17,15 @@ const (
 )
 
 var (
-	defaultBucket = map[float64]float64{
-		0.005: 0.0,
-		0.01:  0.0,
-		0.025: 0.0,
-		0.05:  0.0,
-		0.1:   0.0,
-		0.25:  0.0,
-		0.5:   0.0,
-		1:     0.0,
-		2.5:   0.0,
-		5:     0.0,
-		10:    0.0,
-		20:    0.0,
-		30:    0.0,
-		40:    0.0,
-		50:    0.0,
-	}
+	defaultBuckets = []float64{.005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 10, 20, 30, 40, 50}
 )
 
 type Exporter struct {
 	config *Config
 
 	// profile
-	execTime *prometheus.GaugeVec
-	errors   *prometheus.GaugeVec
+	errors        *prometheus.GaugeVec
+	execHistogram *prometheus.HistogramVec
 
 	// global
 	scrapeTime   *prometheus.GaugeVec
@@ -56,11 +38,6 @@ type Exporter struct {
 func NewVaultExporter(config *Config) *Exporter {
 	e := &Exporter{
 		config: config,
-		execTime: prometheus.NewGaugeVec(prometheus.GaugeOpts{
-			Namespace: config.PGW.Namespace,
-			Name:      "execution_time_bucket",
-			Help:      "Execution time.",
-		}, joinWithLabelsMap([]string{"le", "type", "profile"}, config.PGW.Labels)),
 		scrapeTime: prometheus.NewGaugeVec(prometheus.GaugeOpts{
 			Namespace: config.PGW.Namespace,
 			Name:      "scrape_time",
@@ -81,14 +58,12 @@ func NewVaultExporter(config *Config) *Exporter {
 			Name:      "last_scrape_duration_seconds",
 			Help:      "The last scrape duration.",
 		}, joinWithLabelsMap([]string{}, config.PGW.Labels)),
-		execBucketCounters: make(map[string]map[float64]float64),
-	}
-
-	e.execBucketCounters[fmt.Sprintf("%s/%s", BucketTotal, "all")] = copyMap(defaultBucket)
-	for _, profile := range config.Vault.Profiles {
-		e.execBucketCounters[fmt.Sprintf("%s/%s", BucketAuth, profile.Name)] = copyMap(defaultBucket)
-		e.execBucketCounters[fmt.Sprintf("%s/%s", BucketRead, profile.Name)] = copyMap(defaultBucket)
-		e.execBucketCounters[fmt.Sprintf("%s/%s", BucketWrite, profile.Name)] = copyMap(defaultBucket)
+		execHistogram: prometheus.NewHistogramVec(prometheus.HistogramOpts{
+			Namespace: config.PGW.Namespace,
+			Name:      "execution_time_bucket",
+			Help:      "Execution time.",
+			Buckets:   defaultBuckets,
+		}, joinWithLabelsMap([]string{"type", "profile"}, config.PGW.Labels)),
 	}
 	return e
 }
@@ -111,17 +86,16 @@ func (e *Exporter) AddCounterValues(c *prometheus.CounterVec, val []string) prom
 	return c.WithLabelValues(values...)
 }
 
-func (e *Exporter) send() {
-	for key, bucket := range e.execBucketCounters {
-		data := strings.SplitN(key, "/", 2)
-		if len(data) != 2 {
-			continue
-		}
-		for le, val := range bucket {
-			vals := []string{fmt.Sprintf("%v", le), data[0], data[1]}
-			e.AddGaugeValues(e.execTime, vals).Set(val)
-		}
+func (e *Exporter) AddHistogramValues(h *prometheus.HistogramVec, val []string) prometheus.Observer {
+	var values []string
+	if len(val) != 0 {
+		values = val
 	}
+	values = append(values, labelValues(e.config.PGW.Labels)...)
+	return h.WithLabelValues(values...)
+}
+
+func (e *Exporter) send() {
 	if err := push.Collectors(
 		e.config.PGW.Job,
 		push.HostnameGroupingKey(),
@@ -130,7 +104,7 @@ func (e *Exporter) send() {
 		e.totalScrapes,
 		e.errors,
 		e.duration,
-		e.execTime,
+		e.execHistogram,
 	); err != nil {
 		logrus.Errorf("Could not push to Pushgateway: %v", err)
 	}
@@ -151,7 +125,7 @@ func (e *Exporter) collect(profile *VaultProfile) error {
 		return err
 	}
 	duration = float64(time.Now().UnixNano()-now) / 1000000000
-	e.IncBucketCounter(BucketAuth, profile.Name, duration)
+	e.AddHistogramValues(e.execHistogram, []string{BucketAuth, profile.Name}).Observe(duration)
 
 	// Check write
 	now = time.Now().UnixNano()
@@ -162,7 +136,7 @@ func (e *Exporter) collect(profile *VaultProfile) error {
 		return err
 	}
 	duration = float64(time.Now().UnixNano()-now) / 1000000000
-	e.IncBucketCounter(BucketWrite, profile.Name, duration)
+	e.AddHistogramValues(e.execHistogram, []string{BucketWrite, profile.Name}).Observe(duration)
 
 	// Check read
 	now = time.Now().UnixNano()
@@ -173,20 +147,9 @@ func (e *Exporter) collect(profile *VaultProfile) error {
 		return err
 	}
 	duration = float64(time.Now().UnixNano()-now) / 1000000000
-	e.IncBucketCounter(BucketRead, profile.Name, duration)
+	e.AddHistogramValues(e.execHistogram, []string{BucketRead, profile.Name}).Observe(duration)
 
 	return nil
-}
-
-func (e *Exporter) IncBucketCounter(name, profile string, duration float64) {
-	nameWithProfile := fmt.Sprintf("%s/%s", name, profile)
-	logrus.Debugf("%s = %v", nameWithProfile, duration)
-	for d := range e.execBucketCounters[nameWithProfile] {
-		if duration >= d {
-			logrus.Debugf("Inc %s le=%v", nameWithProfile, d)
-			e.execBucketCounters[nameWithProfile][d]++
-		}
-	}
 }
 
 func (e *Exporter) resetErrorCounters() {
@@ -219,7 +182,7 @@ func (e *Exporter) Collect() {
 			}
 
 			e.AddGaugeValues(e.duration, nil).Set(duration)
-			e.IncBucketCounter(BucketTotal, "all", duration)
+			e.AddHistogramValues(e.execHistogram, []string{BucketTotal, "all"}).Observe(duration)
 
 			for name, bucker := range e.execBucketCounters {
 				logrus.Debugf("Counters %s: %#v", name, bucker)
